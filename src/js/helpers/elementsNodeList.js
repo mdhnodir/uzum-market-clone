41498454ac77c5430ef452fea930365020f9281e
Nodir <nokia29722022@gmail.com>
2023-11-17 00:46:41 +0500

    header

---
+++ src/js/helpers/elementsNodeList.js
@@ -0,0 +1,5 @@
+const html = document.documentElement
+
+export {
+  html,
+}
---
+++ src/js/modules/index.js
@@ -0,0 +1,28 @@
+import { html } from './../helpers/elementsNodeList'
+
+
+// Проверка браузера на поддержку .webp изображений =================================================================================================================
+function isWebp() {
+  // Проверка поддержки webp
+  const testWebp = (callback) => {
+    const webP = new Image()
+
+    webP.onload = webP.onerror = () => callback(webP.height === 2)
+    webP.src =
+      'data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA'
+  }
+  // Добавление класса _webp или _no-webp для HTML
+  testWebp((support) => {
+    const className = support === true ? 'webp' : 'no-webp'
+    html.classList.add(className)
+
+    FLS(support === true ? 'webp поддерживается' : 'webp не поддерживается')
+  })
+}
+
+
+
+export {
+  isWebp,
+  
+}
